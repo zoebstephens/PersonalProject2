6c1db80f8f60db41880975cb6986a6f5398c7078
zoebstephens <zoebstephens@gmail.com>
2022-03-01 10:19:06 -0800

    add tests for methods

--- Program.cs
+++ Program.cs
@@ -28,8 +28,47 @@ namespace PersonalProject2
             bool testGetGuess = TestGetGuess.RunTest();
             Console.WriteLine($"Test GetGuess(correctWord): {testGetGuess}");
 
-            
+
 
         }
+        public static string GetRandomWord()
+        {
+            // 1. Load a filed called `words.txt`
+            // 2. Store each line as a separate word in a list named `words`
+            // 3. Generate a random number between 0 and `words.Count` and store the result in
+            //    a variable called `ix`
+            // 4. Return the word at position `ix`. (e.g. `words[ix]`)
+            return null;
+        }
+
+        public static string GetGuess(string correctWord)
+        {
+            // Prompt the user to make a guess
+            // Read input from the keyboard and store the results in a variable named guess
+            // If guess is the correct length (the guess and random word should be the same length), return the guess.
+            // Otherwise, display an error message and ask them to make another guess.
+            return null;
+        }
+
+        public static string DisplayInfo(string guess, string correct)
+        {
+            // Validate that the guess and correct word are the same length
+            // If the lengths do not match, throw an exception
+            // If they match, iterate through each character:
+            // Use the DisplayCharInfo method to determine what color to print the character
+            return null;
+        }
+
+        public static string DisplayCharInfo(char guess, int pos, string correct)
+        {
+            // If the guess is in the correct position, select the color green.
+            // If the guess is in the correct word but not correct position, select yellow.
+            // If the guess is not in the correct word, select red.
+            // Display the guess
+            // Reset the color back to white before returning
+            return null;
+        }
+
+
     }
 }
--- TestGetGuess.cs
+++ TestGetGuess.cs
@@ -7,38 +7,38 @@ namespace PersonalProject2
         public static bool RunTest()
         {
             Console.WriteLine($"Type in sleep. You should see a green S, and you should see a red L, E, E, P");
-            string input = Program.GetGuess("stunt");
+            string result = Program.GetGuess("stunt");
             string expected = "SLEEP";
-            if (input != expected){
+            if (result != expected){
                 Console.Error.WriteLine($"Expected result to be 'Sleep' but was {result}");
                 return false;
             }
             Console.WriteLine($"Type in sugar. You should see a green S, //& a yellow U, and you should see a red G, A, R");
-            input = Program.GetGuess("stunt");
-            string expected = "SUGAR";
-            if (input != expected){
+            result = Program.GetGuess("stunt");
+            expected = "SUGAR";
+            if (result != expected){
                 Console.Error.WriteLine($"Expected result to be 'Sugar' but was {result}");
                 return false;
             }
             Console.WriteLine($"Type in the word stunt. You should expect all the letters to be green");
-            input = Program.GetGuess("stunt");
+            result = Program.GetGuess("stunt");
 
             Console.WriteLine($"Type in the word joke. You should see an error message due to the length of the word. Now type in jokes. You should see a green O, E, S //& a red J, K");
-            string input2 = Program.GetGuess("cones");
+            string result2 = Program.GetGuess("cones");
             Console.WriteLine($"Type in the word kopes. You should see an error message due to the input of a nonexistent word. Now type boats. You should see a Green O, S //& a Red B, A, T ");
-            input2 = Program.GetGuess("cones");
+            result2 = Program.GetGuess("cones");
             Console.WriteLine($"Type in the word cones. You should expect all letters to be green.");
-            input2 = Program.GetGuess("cones");
+            result2 = Program.GetGuess("cones");
 
             Console.WriteLine($"Type in the word alone. You should see a green A, L, O, //& a red N, E");
-            string input3 = Program.GetGuess("aloha");
-            string expected = "ALONE";
-            if (input != expected){
+            string result3 = Program.GetGuess("aloha");
+            expected = "ALONE";
+            if (result != expected){
                 Console.Error.WriteLine($"Expected result to be 'ALONE' but was {result}");
                 return false;
             }
             Console.WriteLine($"Type in the word aloha. You should expect all letters to be green.");
-            input3 = Program.GetGuess("aloha");
+            result3 = Program.GetGuess("aloha");
 
             
 
